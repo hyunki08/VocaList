76205d39823d5da57422cb339341472a3f1bb9ac
hyunki <hyunki08@gmail.com>
2022-11-02 15:34:28 +0900

    Input 컴포넌트 placeholder 텍스트 컬러 변경

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
-import { TextInput } from 'react-native';
-import { styles } from '../Style';
+import React, { useState } from "react";
+import { TextInput } from "react-native";
+import { styles } from "../Style";
 
 export const Input = ({ placeholder, onEndEditing, onFoucus }) => {
-    const [focused, setFocused] = useState(false);
-    const [value, setValue] = useState("");
-    const endEditing = () => {
-        setFocused(false);
-        onEndEditing(value);
-        onFoucus(false);
-    }
-    return (
-        <TextInput
-            autoCorrect={false}
-            placeholder={placeholder}
-            value={value}
-            onChangeText={text => setValue(text)}
-            style={{ ...styles.input, backgroundColor: focused === true ? "white" : "#DDD" }}
-            onFocus={() => { setFocused(true); onFoucus(true); }}
-            onEndEditing={endEditing}
-        />
-    );
-}
+  const [focused, setFocused] = useState(false);
+  const [value, setValue] = useState("");
+  const endEditing = () => {
+    setFocused(false);
+    onEndEditing(value);
+    onFoucus(false);
+  };
+  return (
+    <TextInput
+      autoCorrect={false}
+      placeholderTextColor="#999"
+      placeholder={placeholder}
+      value={value}
+      onChangeText={(text) => setValue(text)}
+      style={{
+        ...styles.input,
+        backgroundColor: focused === true ? "white" : "#DDD",
+      }}
+      onFocus={() => {
+        setFocused(true);
+        onFoucus(true);
+      }}
+      onEndEditing={endEditing}
+    />
+  );
+};
